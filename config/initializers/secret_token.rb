378092745066393272f1a8f5d4eacc537088035a
Jean-Philippe Gauthier <j-p.g@hotmail.com>
2013-05-18 15:28:34 -0400

    PROJ-72: Initial commit after rails new fcms -T

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fcms::Application.config.secret_token = 'f7a002ae598811054f069bb42229a50da850ed79ff1c8e305a7c78b8deeb8260fa678b0643ca8c83fcf5207fe1f63cfb8c0d830aab1df1b513edf976d448ead1'

038f38f3002df42a43d5611be4893f4bd5c25f3c
Émile Bélair <ebelair@belairsoft.com>
2013-05-22 15:59:50 -0400

    PROJ-33 Added AJAX form submission, page-specific JavaScript in application.html and JS/CSS compression in production

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -1,6 +1,9 @@
 class LoginController < ApplicationController
   def create
-    response = { :authenticated => (params[:user] == 'test' and params[:password] == "root123") }
-    render :json => response
+    if (params[:user] == 'test' and params[:password] == "root123")
+      render :json => {}, :status => :ok
+    else
+      render :json => {}, :status => :unauthorized
+    end
   end
 end

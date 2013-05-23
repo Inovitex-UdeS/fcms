df951a1318f229c0d69cab483846b7097b8d2219
Jean-Philippe Gauthier <j-p.g@hotmail.com>
2013-05-23 00:50:12 -0400

    PROJ-42: Dummy view to log-out & fixed migration  - Removed checkbox inline override in base css for devise  - Mapped USER table to user model  - Fixed inline check box to "remember me"  - Fixed migration to add column instead of dop tabl and recreating it

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ActiveRecord::Base
+
+  self.table_name = "USER"
+
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable,
   # :lockable, :timeoutable and :omniauthable
--- db/migrate/20130522201536_devise_create_users.rb
+++ db/migrate/20130522201536_devise_create_users.rb
@@ -1,44 +1,42 @@
 class DeviseCreateUsers < ActiveRecord::Migration
   def change
-    create_table(:users) do |t|
-      ## Database authenticatable
-      t.string :email,              :null => false, :default => ""
-      t.string :encrypted_password, :null => false, :default => ""
+    ## Database authenticatable
+    add_column :USER, :email, :string, :null => false, :default => ""
+    add_column :USER, :encrypted_password, :string, :null => false, :default => ""
 
-      ## Recoverable
-      t.string   :reset_password_token
-      t.datetime :reset_password_sent_at
+    ## Recoverable
+    add_column :USER, :reset_password_token, :string
+    add_column :USER, :reset_password_sent_at, :datetime
 
-      ## Rememberable
-      t.datetime :remember_created_at
+    ## Rememberable
+    add_column :USER, :remember_created_at, :datetime
 
-      ## Trackable
-      t.integer  :sign_in_count, :default => 0
-      t.datetime :current_sign_in_at
-      t.datetime :last_sign_in_at
-      t.string   :current_sign_in_ip
-      t.string   :last_sign_in_ip
+    ## Trackable
+    add_column :USER, :sign_in_count, :integer, :default => 0
+    add_column :USER, :current_sign_in_at, :datetime
+    add_column :USER, :last_sign_in_at, :datetime
+    add_column :USER, :current_sign_in_ip, :string
+    add_column :USER, :last_sign_in_ip, :string
 
-      ## Confirmable
-      # t.string   :confirmation_token
-      # t.datetime :confirmed_at
-      # t.datetime :confirmation_sent_at
-      # t.string   :unconfirmed_email # Only if using reconfirmable
+    ## Confirmable
+    # t.string   :confirmation_token
+    # t.datetime :confirmed_at
+    # t.datetime :confirmation_sent_at
+    # t.string   :unconfirmed_email # Only if using reconfirmable
 
-      ## Lockable
-      # t.integer  :failed_attempts, :default => 0 # Only if lock strategy is :failed_attempts
-      # t.string   :unlock_token # Only if unlock strategy is :email or :both
-      # t.datetime :locked_at
+    ## Lockable
+    # t.integer  :failed_attempts, :default => 0 # Only if lock strategy is :failed_attempts
+    # t.string   :unlock_token # Only if unlock strategy is :email or :both
+    # t.datetime :locked_at
 
-      ## Token authenticatable
-      # t.string :authentication_token
+    ## Token authenticatable
+    # t.string :authentication_toke
 
+    add_column :USER, :created_at, :datetime
+    add_column :USER, :updated_at, :datetime
 
-      t.timestamps
-    end
-
-    add_index :users, :email,                :unique => true
-    add_index :users, :reset_password_token, :unique => true
+    add_index :USER, :email,                :unique => true
+    add_index :USER, :reset_password_token, :unique => true
     # add_index :users, :confirmation_token,   :unique => true
     # add_index :users, :unlock_token,         :unique => true
     # add_index :users, :authentication_token, :unique => true

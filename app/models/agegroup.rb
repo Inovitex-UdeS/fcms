b7c7aaf3c8b7a250ef35f548eca6cdba446dfdeb
freemind <m.paquette@inovitex.com>
2013-05-24 15:59:31 +0000

    PROJ-91 model update

--- app/models/agegroup.rb
+++ app/models/agegroup.rb
@@ -1,3 +1,7 @@
 class Agegroup < ActiveRecord::Base
-  # attr_accessible :title, :body
+  attr_accessible :min, :max, :description, :fee, :max_duration
+
+  #relations
+  belongs_to :category
+  belongs_to :edition
 end

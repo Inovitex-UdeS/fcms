07b8b87b65f9c6583ab859868538b4bc127ba6b9
Jean-Philippe Gauthier <j-p.g@hotmail.com>
2013-05-24 11:04:37 -0400

    PROJ-72: Follow Rails standard in database  - Changed database to follow rails conventions

--- db/migrate/20130522201536_devise_create_users.rb
+++ db/migrate/20130522201536_devise_create_users.rb
@@ -1,22 +1,22 @@
 class DeviseCreateUsers < ActiveRecord::Migration
   def change
     ## Database authenticatable
-    add_column :USER, :email, :string, :null => false, :default => ""
-    add_column :USER, :encrypted_password, :string, :null => false, :default => ""
+    add_column :users, :email, :string, :null => false, :default => ""
+    add_column :users, :encrypted_password, :string, :null => false, :default => ""
 
     ## Recoverable
-    add_column :USER, :reset_password_token, :string
-    add_column :USER, :reset_password_sent_at, :datetime
+    add_column :users, :reset_password_token, :string
+    add_column :users, :reset_password_sent_at, :datetime
 
     ## Rememberable
-    add_column :USER, :remember_created_at, :datetime
+    add_column :users, :remember_created_at, :datetime
 
     ## Trackable
-    add_column :USER, :sign_in_count, :integer, :default => 0
-    add_column :USER, :current_sign_in_at, :datetime
-    add_column :USER, :last_sign_in_at, :datetime
-    add_column :USER, :current_sign_in_ip, :string
-    add_column :USER, :last_sign_in_ip, :string
+    add_column :users, :sign_in_count, :integer, :default => 0
+    add_column :users, :current_sign_in_at, :datetime
+    add_column :users, :last_sign_in_at, :datetime
+    add_column :users, :current_sign_in_ip, :string
+    add_column :users, :last_sign_in_ip, :string
 
     ## Confirmable
     # t.string   :confirmation_token
@@ -31,12 +31,9 @@ class DeviseCreateUsers < ActiveRecord::Migration
 
     ## Token authenticatable
     # t.string :authentication_toke
-
-    add_column :USER, :created_at, :datetime
-    add_column :USER, :updated_at, :datetime
-
-    add_index :USER, :email,                :unique => true
-    add_index :USER, :reset_password_token, :unique => true
+    
+    add_index :users, :email,                :unique => true
+    add_index :users, :reset_password_token, :unique => true
     # add_index :users, :confirmation_token,   :unique => true
     # add_index :users, :unlock_token,         :unique => true
     # add_index :users, :authentication_token, :unique => true

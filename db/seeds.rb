df2012bee44474de6e3ac4e7ddbae761c056d352
Laurens Coderre <lcoderre@me.com>
2013-05-27 11:13:59 -0400

    Upgraded db seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,6 +9,7 @@
 # Users
 user1 = User.create(last_name: 'Gauthier', first_name: 'Jean-Philippe', telephone: '819-843-7004', address: '112 rue rene', city: 'Magog', province: 'Quebec', gender: true, postal_code: 'J1X3W5', birthday: '1991-07-29', email: 'j-p.g@hotmail.com', password: 'password')
 user2 = User.create(last_name: 'Paquette', first_name: 'Daniel', telephone: '111-111-1111', address: '1111 rue argyll', city: 'Sherbrooke', province: 'Quebec', gender: true, postal_code: 'J1Z8V4', birthday: '1980-05-12', email: 'dp@me.com', password: 'password')
+user3 = User.create(last_name: 'Mine', first_name: 'Ad', telephone: '911', address: '007 thugstreet', city: 'Sherbrooke', province: 'Quebec', gender: true, postal_code: 'J1Z8V4', birthday: '1980-05-12', email: 'admin@admin.com', password: 'password')
 
 # Roles
 role1 = Role.create(name: 'participant')
@@ -18,15 +19,41 @@ role3 = Role.create(name: 'administrateur')
 # Users_Roles
 user1.roles << role1
 user2.roles << role2
+user3.roles << role3
 
 # Edition
 edition1 = Edition.create(year: 2007, limit_date: '2007-02-01')
 
 # Categories
-category1 = Category.create(name: 'Guitare repertoire', nb_perf_min: 2, nb_perf_max: 4, description:'Categorie pour le repertoire de la guitare classique')
+category1 = Category.create(name: 'Répertoire', nb_perf_min: 2, nb_perf_max: 4, description:'Categorie pour le repertoire de la guitare classique')
+category2 = Category.create(name: 'Musique Canadienne', nb_perf_min: 2, nb_perf_max: 4, description:'')
+category3 = Category.create(name: 'Festival', nb_perf_min: 2, nb_perf_max: 4, description:'')
+category4 = Category.create(name: 'Récital', nb_perf_min: 2, nb_perf_max: 4, description:'')
+category5 = Category.create(name: 'Musique de chambre / Ensemble vocal', nb_perf_min: 2, nb_perf_max: 4, description:'')
+category6 = Category.create(name: 'Concerto / Concertino', nb_perf_min: 2, nb_perf_max: 4, description:'')
+category7 = Category.create(name: 'Concerto OSJS', nb_perf_min: 2, nb_perf_max: 4, description:'')
 
 # AgeGroups
-agegroup1 = Agegroup.create(edition_id: edition1.id, category_id: category1.id, min: '1992-07-01', max: '1990-07-01', fee: 38, max_duration: 7)
+agegroup1 = Agegroup.create(edition_id: edition1.id, category_id: category1.id, min: '1992-07-01', max: '1990-07-01', fee: 33, max_duration: 7)# 7-9 ans
+agegroup2 = Agegroup.create(edition_id: edition1.id, category_id: category1.id, min: '1992-07-01', max: '1990-07-01', fee: 36, max_duration: 7)# 10-11 ans
+agegroup3 = Agegroup.create(edition_id: edition1.id, category_id: category1.id, min: '1992-07-01', max: '1990-07-01', fee: 38, max_duration: 7)# 12-13 ans
+agegroup4 = Agegroup.create(edition_id: edition1.id, category_id: category1.id, min: '1992-07-01', max: '1990-07-01', fee: 39, max_duration: 7)# 14-16 ans
+agegroup5 = Agegroup.create(edition_id: edition1.id, category_id: category1.id, min: '1992-07-01', max: '1990-07-01', fee: 40, max_duration: 7)# 17+ ans
+
+agegroup6 = Agegroup.create(edition_id: edition1.id, category_id: category6.id, min: '1992-07-01', max: '1990-07-01', fee: 40, max_duration: 7)# 11- ans
+agegroup7 = Agegroup.create(edition_id: edition1.id, category_id: category6.id, min: '1992-07-01', max: '1990-07-01', fee: 50, max_duration: 7)# 12-17 ans
+
+agegroup8 = Agegroup.create(edition_id: edition1.id, category_id: category3.id, min: '1992-07-01', max: '1990-07-01', fee: 25, max_duration: 7) # 7-9 ans
+agegroup9 = Agegroup.create(edition_id: edition1.id, category_id: category3.id, min: '1992-07-01', max: '1990-07-01', fee: 27, max_duration: 7) # 10-11 ans
+agegroup10 = Agegroup.create(edition_id: edition1.id, category_id: category3.id, min: '1992-07-01', max: '1990-07-01', fee:30, max_duration: 7)# 12-13 ans
+agegroup11 = Agegroup.create(edition_id: edition1.id, category_id: category3.id, min: '1992-07-01', max: '1990-07-01', fee:32, max_duration: 7)# 14-16 ans
+agegroup12 = Agegroup.create(edition_id: edition1.id, category_id: category3.id, min: '1992-07-01', max: '1990-07-01', fee:35, max_duration: 7)# 17+ ans
+
+agegroup13 = Agegroup.create(edition_id: edition1.id, category_id: category4.id, min: '1992-07-01', max: '1990-07-01', fee:55, max_duration: 7)# 15-16 ans
+agegroup14 = Agegroup.create(edition_id: edition1.id, category_id: category4.id, min: '1992-07-01', max: '1990-07-01', fee:65, max_duration: 7)# 17+ ans
+
+agegroup15 = Agegroup.create(edition_id: edition1.id, category_id: category5.id, min: '1992-07-01', max: '1990-07-01', fee:20, max_duration: 7)# 7-11 ans
+agegroup16 = Agegroup.create(edition_id: edition1.id, category_id: category5.id, min: '1992-07-01', max: '1990-07-01', fee:25, max_duration: 7)# 12+ ans 
 
 # Composers
 composer1 = Composer.create(name: 'SOR F.')

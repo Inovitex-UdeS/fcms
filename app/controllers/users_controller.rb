7d200734ca7b9c5407677138492fb82ae0eb215f
Emilien Boisvert <emilienboisvert@me.com>
2013-06-06 14:04:32 -0400

    Changes made to he user profile page
    - Added a DropDown list for the city
    - Made changed to tests, still not woking for the profile page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,10 +2,14 @@ class UsersController < ApplicationController
 
   def edit
     @user = User.find(params[:id])
+    @user.contactinfo ||= Contactinfo.new
+    @user.contactinfo.city ||= City.new
   end
 
   def update
     @user = User.find(params[:id])
+    #city = City.find(:name => @user.contactinfo.city.)
+    #@user.contactinfo.city.update_attributes(params[:city])
     if @user.update_attributes(params[:user])
       flash[:success] = "Mise a jour du profil"
       sign_in @user
--- features/step_definitions/sign_in.rb
+++ features/step_definitions/sign_in.rb
@@ -13,13 +13,16 @@ Given /^I exist as a user$/ do
   @city = City.create!({:name => "TestCity", :created_at => DateTime.now, :updated_at => DateTime.now})
 
   @co = Contactinfo.create!({:telephone => "819-993-4995", :address => "666 Hellstreet", :postal_code => "J1K1C5", :province => "Quebec",
-                          :created_at => DateTime.now, :updated_at => DateTime.now, :city_id => @city.id})
+                         :created_at => DateTime.now, :updated_at => DateTime.now, :city_id => @city.id})
 
-  @u = User.create!({:email=>"tests@inovitex.com", :last_name=>"Cucumber", :first_name=>"Tests", :birthday=>"2011-03-01",
-                :password=>"123test123", :password_confirmation=>"123test123",
-                :confirmed_at => DateTime.now, :contactinfo_id => @co.id})
+  @u = User.create(id: 200, last_name: 'Cucumber', first_name: 'Tests', gender: true, birthday: '2011-03-01',
+                   email: 'tests@inovitex.com', password: '123test123', confirmed_at: '2013-05-28 02:01:11.70392', :contactinfo_id => @co.id)
+  begin
+    lol = 1
+  rescue    Exception
+    lol = 1
 
-  @u.contactinfo = @co
-  @co.city = @city
+  end
+
 
 end

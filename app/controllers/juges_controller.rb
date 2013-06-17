3d3c60a4031934de963af58657ec3ba572216a0a
huaa3001 <alexishuard@gmail.com>
2013-06-17 10:54:31 -0400

    PROJ-122: Added Juge form in Administration panel'

--- app/controllers/juges_controller.rb
+++ app/controllers/juges_controller.rb
@@ -8,7 +8,34 @@ class JugesController < ApplicationController
 
   def create
     begin
-      @juge = User.find(1)
+      first_name  = params[:user][:first_name]
+      last_name   = params[:user][:last_name]
+      birthday    = params[:user][:birthday]
+      email       = params[:user][:email]
+
+      telephone   = params[:user][:contactinfo_attributes][:telephone]
+      address     = params[:user][:contactinfo_attributes][:address]
+      postal_code = params[:user][:contactinfo_attributes][:postal_code].gsub(/ /,'')
+      province    = params[:user][:contactinfo_attributes][:province]
+
+      city_name   = params[:user][:contactinfo_attributes][:city_attributes][:name]
+      city        = City.where(:name => city_name).first.id
+
+      contactinfo = Contactinfo.create(telephone: telephone, address: address, city_id: city, province: province, postal_code: postal_code)
+
+      @juge = User.create(last_name: last_name, first_name: first_name, gender: true, birthday: birthday, email: email, password: 'password', contactinfo_id: contactinfo.id, confirmed_at: Time.now)
+      @juge.contactinfo = contactinfo
+
+      if @juge.save
+        role = Role.where(:name => 'juge').first.id
+        @juge.role << role
+        render :json => @juge
+      else
+        render :json => { :errors => "Error creating User" }, :status => 422
+      end
+
+    rescue
+      render :json => { :errors => "Erreurs" }, :status => 422
     end
   end
 end

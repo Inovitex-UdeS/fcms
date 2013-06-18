3e0689391219bb1d24c180f270e2054aeceab5f1
huaa3001 <alexishuard@gmail.com>
2013-06-18 10:44:21 -0400

    PROJ-122: Added juge role association to new entries

--- app/controllers/juges_controller.rb
+++ app/controllers/juges_controller.rb
@@ -15,20 +15,22 @@ class JugesController < ApplicationController
 
       telephone   = params[:user][:contactinfo_attributes][:telephone]
       address     = params[:user][:contactinfo_attributes][:address]
+      address2    = params[:user][:contactinfo_attributes][:address2]
       postal_code = params[:user][:contactinfo_attributes][:postal_code].gsub(/ /,'')
       province    = params[:user][:contactinfo_attributes][:province]
 
       city_name   = params[:user][:contactinfo_attributes][:city_attributes][:name]
       city        = City.where(:name => city_name).first.id
 
-      contactinfo = Contactinfo.create(telephone: telephone, address: address, city_id: city, province: province, postal_code: postal_code)
+      contactinfo = Contactinfo.create(telephone: telephone, address: address, address2: address2, city_id: city, province: province, postal_code: postal_code)
 
       @juge = User.create(last_name: last_name, first_name: first_name, gender: true, birthday: birthday, email: email, password: 'password', contactinfo_id: contactinfo.id, confirmed_at: Time.now)
       @juge.contactinfo = contactinfo
 
+      role = Role.where(:name => 'juge').first
+      @juge.roles  << role
+
       if @juge.save
-        role = Role.where(:name => 'juge').first.id
-        @juge.role << role
         render :json => @juge
       else
         render :json => { :errors => "Error creating User" }, :status => 422

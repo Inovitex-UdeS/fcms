21759ebd5e413733c334f9e62714c5c6ab065066
Jean-Philippe Gauthier <j-p.g@hotmail.com>
2013-06-18 22:24:39 -0400

    PROJ-121: Added cities  - Added functions to controller  - Added partials views  - Added JS init Datatables  - Removed Typeahead test

--- app/assets/javascripts/cities.js
+++ app/assets/javascripts/cities.js
@@ -1,20 +1,8 @@
+// Place all the behaviors and hooks related to the matching controller here.
+// All this logic will automatically be available in application.js.
+
+//= require datatables
+
 $(document).ready(function(){
-
-    var labels, mapped
-    $("#input-cities").typeahead({
-        source: function (query, process) {
-            $.get('/autocomplete/cities', { q: query }, function (data) {
-                labels = []
-                mapped = {}
-
-                $.each(data, function (i, item) {
-                    mapped[item.label] = item.value
-                    labels.push(item.label)
-                })
-
-                process(labels)
-            })
-        }
-    });
-
+    fcms.initTable($('form'));
 });
--- app/controllers/cities_controller.rb
+++ app/controllers/cities_controller.rb
@@ -1,10 +1,52 @@
+#encoding: utf-8
 class CitiesController < ApplicationController
 
-  def show
+  def new
     @city = City.new
+    @cities = City.all
+  end
 
-    respond_to do |format|
-      format.html
+  def create
+    begin
+      @city = City.new(params[:edition])
+      if @city.save
+        render :json => @city
+      else
+        render :json => {:message => @city.errors.full_messages, :status => 422}.to_json
+      end
+    rescue
+      render :json => {:message => @city.errors.full_messages, :status => 422}.to_json
+    end
+  end
+
+  def destroy
+    begin
+      @city = City.find(params[:id])
+      if @city
+        @city.destroy
+        render :json => {:message => "La ville a été supprimée avec succès", :status => 200 }.to_json
+      else
+        render :json => {:message => "La ville n'a pas été trouvée", :status => 404}.to_json
+      end
+    rescue
+      render :json => {:message => "Erreur lors de la suppression de la ville", :status => 404}.to_json
+    end
+  end
+
+  def update
+    begin
+      @city = City.find(params[:id])
+      if @city
+        if @city.update_attributes(params[:city])
+          render :json => @city
+        else
+          render :json =>{:message => "L'édition n'a pu être mis à jour", :status => 404}.to_json
+        end
+      else
+        render :json => {:message =>  "L'édition n'a pas été trouvé", :status => 404}.to_json
+      end
+    rescue
+      render :json => {:message => "Erreur lors de la mise à jour de l'édition", :status => 404}.to_json
     end
   end
 

0d53756f2c381fa2102c072777aa3ab278d560ae
freemind <m.paquette@inovitex.com>
2013-07-16 09:37:17 -0400

    SMTP configuration in production environment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,4 +68,15 @@ Fcms::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
+
+  # SMTP configuration
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+      address:              'smtp.usherbrooke.ca',
+      port:                 25,
+      domain:               'fcmsherbrooke.com',
+      user_name:            'portail@fcmsherbrooke.com',
+      password:             '',
+      authentication:       '',
+      enable_starttls_auto: false  }
 end

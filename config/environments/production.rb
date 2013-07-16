95f8036bf16443d4eb4564afa9b87630050d79bd
freemind <m.paquette@inovitex.com>
2013-07-16 10:10:54 -0400

    Working production environment smtp config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -70,16 +70,11 @@ Fcms::Application.configure do
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
 
   # SMTP configuration
-  #config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default_url_options = { :host => 'portail.fcmsherbrooke.com' }
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
       address:              'smtp.usherbrooke.ca',
-      port:                 25,
-      domain:               'fcmsherbrooke.com',
-      user_name:            'portail@fcmsherbrooke.com',
-      password:             '',
-      authentication:       '',
-      enable_starttls_auto: false  }
+      port:                 25 }
 end

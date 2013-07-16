079379fbf09494ee11363b9a7c8049708676896a
freemind <m.paquette@inovitex.com>
2013-07-16 09:51:53 -0400

    Missing lines to the production smtp config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -70,6 +70,9 @@ Fcms::Application.configure do
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
 
   # SMTP configuration
+  #config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_url_options = { :host => 'portail.fcmsherbrooke.com' }
+
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
       address:              'smtp.usherbrooke.ca',

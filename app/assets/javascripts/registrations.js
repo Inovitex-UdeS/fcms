ef408176ea92d0ed877216584e6a68be2c2d87bd
Jean-Philippe Gauthier <j-p.g@hotmail.com>
2013-07-23 22:16:43 -0400

    PROJ-151: Added noob fadeIn on registration field

--- app/assets/javascripts/registrations.js
+++ app/assets/javascripts/registrations.js
@@ -39,8 +39,8 @@ function changeCategory(category_id) {
         if (maxDuration == null) fcms.showMessage('Vous ne correspondez pas à une catégorie d\'âge, vous n\'avez donc pas le droit de vous inscrire dans cette classe', 3);
         $('input[type=submit]').attr('disabled', (maxDuration == null));
 
-        if (maxDuration != null) $('.unit_duration').attr('max', maxDuration)
-        else  $('.unit_duration').attr('max', 0)
+        if (maxDuration != null) $('.unit_duration').attr('max', maxDuration);
+        else  $('.unit_duration').attr('max', 0);
     });
 }
 
@@ -110,8 +110,9 @@ $(document).on('nested:fieldAdded:performances', function(event){
         }
     });
 
-    if (maxDuration != null) $(event.field.children().find('.unit_duration')).attr('max', maxDuration)
-    else  $(event.field.children().find('.unit_duration')).attr('max', 0)
+    if (maxDuration != null) $(event.field.children().find('.unit_duration')).attr('max', maxDuration);
+    else  $(event.field.children().find('.unit_duration')).attr('max', 0);
+    $(event.field).hide().fadeIn("slow");
 });
 
 $(document).on('nested:fieldRemoved:performances', function(event){
@@ -149,6 +150,7 @@ $(document).on('nested:fieldAdded:registrations_users', function(event){
         }
     });
     $('#totUsers').text($('#users .fields').length);
+    $(event.field).hide().fadeIn("slow");
 });
 
 $(document).on('nested:fieldRemoved:registrations_users', function(event){

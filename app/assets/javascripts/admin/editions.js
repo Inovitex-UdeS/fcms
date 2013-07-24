bb4e6bd1f58b5f4d7924f56a7aa16294e246f88a
Jean-Philippe Gauthier <j-p.g@hotmail.com>
2013-07-24 16:19:13 -0400

    PROJ-151: Current edition changed message

--- app/assets/javascripts/admin/editions.js
+++ app/assets/javascripts/admin/editions.js
@@ -28,11 +28,11 @@ $(document).ready(function() {
     fcms.initTable();
     fcms.bindForm($('#new_edition'), 3);
 
-    $('#edit_edition_2').on('ajax:success', function(evt, data, status, xhr) {
+    $('form[action="/admin/currenteditions"]').on('ajax:success', function(evt, data, status, xhr) {
         fcms.showMessage('La mise à jour de l\'édition courante a été complétée avec succès!');
     });
 
-    $('#edit_edition_2').on('ajax:error', function(event, xhr, status) {
+    $('form[action="/admin/currenteditions"]').on('ajax:error', function(event, xhr, status) {
         fcms.showMessage(xhr.responseText, 3);
     });
 });

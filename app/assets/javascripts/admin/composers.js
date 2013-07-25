7ac99caaeaabc73a0a88dbae6ea15243ece9047e
Jean-Philippe Gauthier <j-p.g@hotmail.com>
2013-07-24 21:07:48 -0400

    PROJ-151: Dynamic datatables for composers and pieces

--- app/assets/javascripts/admin/composers.js
+++ app/assets/javascripts/admin/composers.js
@@ -4,7 +4,21 @@
 //= require datatables
 
 $(document).ready(function(){
-    fcms.bindTable($('.table'));
-    fcms.initTable();
+
+    var tsDTOptions = {
+        "bInfo": false,
+        "bProcessing": true,
+        "bServerSide": true,
+        "sAjaxSource": $('#composers-table').data('source'),
+        "fnDrawCallback": function( oSettings ) {
+            // Add a click handler to the rows (selectable rows)
+            $.each(oTable.fnGetNodes(), function() {
+                $(this).single_double_click(fcms.fnSelectableRows, fcms.fnEditableRows);
+            });
+        }
+    };
+
+    fcms.bindTable($('#composers-table'));
+    fcms.initTable(tsDTOptions);
     fcms.bindForm($('form'), 3);
 });

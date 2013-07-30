4ba31b588bbff5b7a1e9d1c7d51d2708734b9e09
Jean-Philippe Gauthier <j-p.g@hotmail.com>
2013-07-29 20:53:14 -0400

    PROJ-151: Fixed delete modal window on specifics forms

--- app/assets/javascripts/datatables.js
+++ app/assets/javascripts/datatables.js
@@ -427,34 +427,3 @@ fcms.confirmRegDel = function (obj) {
 
     confirmModal.modal('show');
 };
-
-fcms.confirm = function (heading, question, callback) {
-
-    var confirmModal =
-        $('<div class="modal hide fade">' +
-            '<div class="modal-header">' +
-            '<a class="close" data-dismiss="modal" >&times;</a>' +
-            '<h3>' + heading +'</h3>' +
-            '</div>' +
-
-            '<div class="modal-body">' +
-            '<p>' + question + '</p>' +
-            '</div>' +
-
-            '<div class="modal-footer">' +
-            '<a href="#" class="btn" data-dismiss="modal">' +
-                'Fermer' +
-            '</a>' +
-            '<a href="#" id="okButton" class="btn btn-primary">' +
-                'Supprimer' +
-            '</a>' +
-            '</div>' +
-            '</div>');
-
-    confirmModal.find('#okButton').click(function(event) {
-        callback();
-        confirmModal.modal('hide');
-    });
-
-    confirmModal.modal('show');
-};

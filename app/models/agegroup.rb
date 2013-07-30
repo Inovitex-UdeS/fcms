496e72ee5f88d1e69c688420051a59cf82ddedb8
Jean-Philippe Gauthier <j-p.g@hotmail.com>
2013-07-30 14:00:11 -0400

    DOCUMENTATION: Finished agegroup and category models

--- app/models/agegroup.rb
+++ app/models/agegroup.rb
@@ -1,10 +1,35 @@
+##
+# This class represents the data from agegroups table in database.
+# Contains all the agegroups in application ('8 à 10 ans, 10 à 12 ans, ...')
 class Agegroup < ActiveRecord::Base
+  ##
+  # Agegroup requires an edition to be created
+  #
+  # @ return [String]
   validates :edition_id, :presence => true
+
+  ##
+  # Agegroup requires a cateogry to be created
+  #
+  # @ return [Integer]
   validates :category_id, :presence => true
+
+  ##
+  # Agegroup requires a description to be created
+  #
+  # @ return [String]
   validates :description, :length =>  { :maximum => 128 }
 
-  # Association macros
+  ##
+  # Get the associated category
+  #
+  # @ return [Category]
   belongs_to :category
+
+  ##
+  # Get the associated edition
+  #
+  # @ return [Edition]
   belongs_to :edition
 
 end
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,9 +1,30 @@
+##
+# This class represents the data from categories table in database.
+# Contains all the categories in application ('Répertoire, Musique Canadienne, ...')
 class Category < ActiveRecord::Base
+
+  ##
+  # Agegroup requires an edition to be created
+  #
+  # @ return [String]
   validates :name, :presence => true,  :uniqueness => true,  :length =>  { :maximum => 128 }
 
-  # Association macros
+  ##
+  # Get the associated age groups of this category
+  #
+  # @ return [Agegroup]
   has_many :agegroups
+
+  ##
+  # Get the associated Registrations of this category
+  #
+  # @ return [Registration]
   has_many :registrations
+
+  ##
+  # Get the associated Timeslots of this category
+  #
+  # @ return [Timeslot]
   has_many :timeslots
 
 end
